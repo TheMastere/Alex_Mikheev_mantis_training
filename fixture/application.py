eb6164eede2337765aad4aeaf9aef9702cf8da53
The Master <zoom906@gmail.com>
2020-06-30 23:20:35 +0300

    Test to create a new account

--- fixture/application.py
+++ fixture/application.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "Firefox":
             self.wd = webdriver.Firefox()
         elif browser == "Chrome":
@@ -15,8 +16,10 @@ class Application:
             raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(3)
         self.session = SessionHelper(self)
+        self.james = JamesHelper(self)
+        self.config = config
         self.project = ProjectHelper(self)
-        self.base_url = base_url
+        self.base_url = config['web']['baseUrl']
 
     def is_valid(self):
         try:
