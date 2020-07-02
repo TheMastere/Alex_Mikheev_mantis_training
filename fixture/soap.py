cc44d4713fa6220e9dbfb287beb53c8e14d4f4af
The Master <zoom906@gmail.com>
2020-07-02 16:26:45 +0300

    Test 26. Fixed soap.py add web service address with mistakes

--- fixture/soap.py
+++ fixture/soap.py
@@ -7,9 +7,10 @@ class SoapHelper:
 
     def __init__(self, app):
         self.app = app
+        self.base_url = self.app.config['web']['baseUrl'] + "/api/soap/mantisconnect.php?wsdl"
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.base_url)
         try:
             client.service.mc_login(username, password)
             return True
@@ -17,7 +18,7 @@ class SoapHelper:
             return False
 
     def get_project_list(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.base_url)
         try:
             list_of_project = client.service.mc_projects_get_user_accessible(username, password)
             projects = []
